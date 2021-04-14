bdb53cdb54adb1322432cc7d0c6f8591b3e56a8a
Alejandro Mousist <alejandro.mousist@thalesaleniaspace.com>
2021-04-14 09:50:48 +0200

    Experiment path fix

--- experiments/lsts_rfcn/lsts_end2end_test.py
+++ experiments/lsts_rfcn/lsts_end2end_test.py
@@ -9,20 +9,12 @@ import sys
 os.environ['PYTHONUNBUFFERED'] = '1'
 os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
 os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
-
-#os.environ['MXNET_PROFILER_AUTOSTART'] = '1'
-#os.environ['MXNET_EXEC_BULK_EXEC_TRAIN'] = '0'
-#os.environ['MXNET_EXEC_BULK_EXEC_INFERENCE'] = 'false'
-
-
 this_dir = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(this_dir, '..', '..', 'impression_rfcn'))
+sys.path.insert(0, os.path.join(this_dir, '..', '..', 'lsts_rfcn'))
 
 # import matplotlib
 # matplotlib.use('Agg')
-import impression_train_end2end
-import impression_test
 
+import lsts_network_test
 if __name__ == "__main__":
-    impression_train_end2end.main()
-    #impression_test.main()
+    lsts_network_test.main()
